fca038c3655f036ec3f6c333265d4d2fcf0ef518
Can-Github <84772309+Can-Github@users.noreply.github.com>
2021-05-26 08:30:41 +0300

    Update PB.ts

--- PB.ts
+++ PB.ts
@@ -16,12 +16,12 @@ namespace kodlar {
 
     //% block
     export function mesafe_pini_p8() {
-    pins.digitalWritePin(DigitalPin.P6, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P6, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
     control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P6, 0)
-    mesafe = pins.pulseIn(DigitalPin.P5, PulseValue.High) / 58
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    mesafe = pins.pulseIn(DigitalPin.P7, PulseValue.High) / 58
     basic.showNumber(mesafe)
     basic.pause(100)
     }
