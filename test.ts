50058d50d50282d2575d282422aae4627e72da23
Can-Github <84772309+Can-Github@users.noreply.github.com>
2021-06-17 11:04:12 +0300

    Update main.blocks, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(function () {
+    pinooBit.led(DigitalPin.P0, 0)
+    basic.pause(500)
+    pinooBit.led(DigitalPin.P0, 1)
+    basic.pause(500)
+})
