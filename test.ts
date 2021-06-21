be8c4202095d3c947a93e5b2ac50a08d21fae8bf
Can-Github <84772309+Can-Github@users.noreply.github.com>
2021-06-21 14:37:50 +0300

    Delete test.ts

--- test.ts
+++
@@ -1,7 +0,0 @@
-// tests go here; this will not be compiled when this package is used as an extension.
-basic.forever(function () {
-    pinooBit.led(DigitalPin.P0, 0)
-    basic.pause(500)
-    pinooBit.led(DigitalPin.P0, 1)
-    basic.pause(500)
-})
